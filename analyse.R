1537d15f92685367c4bb01d873932460ade88581
James Siddle <j.siddle@digital-science.com>
2016-06-22 08:34:35 +0100

    More distributions

--- analyse.R
+++ analyse.R
@@ -17,10 +17,25 @@ plot(ss_data$owners,
      ylab="players_forever",
      log = "xy")  
 
+# Display the histogram of player / owner ratios
+ss_data$po_ratio <- ss_data$players_forever / ss_data$owners
+hist(ss_data$po_ratio, breaks=150)
 
+# Get the most highly played games, according to this data set
+best_po_ratio = ss_data[ order(ss_data$po_ratio, decreasing = TRUE), ]
+best_po_ratio = best_po_ratio[best_po_ratio$po_ratio < 1.01,]
+head(best_po_ratio[,c("appid","name","po_ratio","players_forever","owners")], 200)
+# OMSI 2 -> high ratio, specialist game!
+
+# Create table of ranked publishers
 pub_tab <- as.data.frame(table(ss_data$publisher));
 pub_tab_gt2    = pub_tab[pub_tab$Freq > 2, ]
 pub_tab_ranked = pub_tab_gt2[order(pub_tab_gt2$Freq, decreasing = TRUE), ]
 
 hist(pub_tab_ranked$Freq, breaks=50)
 head(pub_tab_ranked,30)
+
+# Histogram of average play times (log)
+# Beautiful normal distribution
+hist(log(ss_data$avg_playtime_forever))
+
