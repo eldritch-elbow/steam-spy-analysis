14375ba65e3bfd668509d754bfd0b55a8d2a6b47
James Siddle <j.siddle@digital-science.com>
2016-06-23 08:56:34 +0100

    Curious scatter plot

--- analyse.R
+++ analyse.R
@@ -1,10 +1,17 @@
 # Set working directory to your workspace ...
 setwd("/my/workspace/steam-spy-analysis")
 
-# Read and display data
+# Read data
 ss_data = read.csv("output/steamspy_out.csv") 
 ss_data
 
+# Basic histograms
+hist(log(ss_data$owners), breaks=100)
+hist(log(ss_data$players_forever), breaks=100)
+hist(log(ss_data$avg_playtime_forever), breaks=50)   # Beautiful normal distribution!
+mean(log(ss_data$owners[ ss_data$owners != 0 ]))
+
+# Basic scatter plots (applying log)
 plot(ss_data$players_forever,
      ss_data$avg_playtime_forever,
      xlab="players_forever", 
@@ -17,17 +24,27 @@ plot(ss_data$owners,
      ylab="players_forever",
      log = "xy")  
 
-# Display the histogram of player / owner ratios
+# Analyse player/owner ratios (candidate top value games)
 ss_data$po_ratio <- ss_data$players_forever / ss_data$owners
-hist(ss_data$po_ratio, breaks=150)
+ss_data_pos = ss_data[complete.cases(ss_data),]     # Filter out rows with NaN p/o ratio
+hist(ss_data_pos$po_ratio, breaks=150)
 
-# Get the most highly played games, according to this data set
-best_po_ratio = ss_data[ order(ss_data$po_ratio, decreasing = TRUE), ]
-best_po_ratio = best_po_ratio[best_po_ratio$po_ratio < 1.01,]
-head(best_po_ratio[,c("appid","name","po_ratio","players_forever","owners")], 200)
+best_po_ratio = ss_data_pos[ order(ss_data_pos$po_ratio, decreasing = TRUE), ]
+best_po_ratio = best_po_ratio[best_po_ratio$owners > 10000,]
+best_po_ratio = best_po_ratio[best_po_ratio$po_ratio < 1.00,]
+best_po_ratio = best_po_ratio[best_po_ratio$po_ratio > 0.95,]
+best_po_ratio[ order(best_po_ratio$avg_playtime_forever, decreasing = TRUE), ]
+
 # OMSI 2 -> high ratio, specialist game!
 
-# Create table of ranked publishers
+plot(ss_data$avg_playtime_forever,
+     ss_data$po_ratio,
+     xlab="avg_playtime_forever", 
+     ylab="po_ratio",
+     log = "x")  
+
+
+# Rank publishers
 pub_tab <- as.data.frame(table(ss_data$publisher));
 pub_tab_gt2    = pub_tab[pub_tab$Freq > 2, ]
 pub_tab_ranked = pub_tab_gt2[order(pub_tab_gt2$Freq, decreasing = TRUE), ]
@@ -35,7 +52,4 @@ pub_tab_ranked = pub_tab_gt2[order(pub_tab_gt2$Freq, decreasing = TRUE), ]
 hist(pub_tab_ranked$Freq, breaks=50)
 head(pub_tab_ranked,30)
 
-# Histogram of average play times (log)
-# Beautiful normal distribution
-hist(log(ss_data$avg_playtime_forever))
 
